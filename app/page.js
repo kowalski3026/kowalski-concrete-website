3a7ef66b342b09af3a553b67bcab91fc2b401da6
kowalski3026 <kowalski3026@gmail.com>
2026-05-08 20:43:06 -0400

    Update page.js

--- app/page.js
+++ app/page.js
@@ -28,7 +28,7 @@ export default function Home() {
 
         </div>
       </nav>
-     
+     <main className="min-h-screen bg-zinc-100 text-zinc-900">
     <section
   className="relative min-h-screen bg-cover bg-center text-white"
   style={{
