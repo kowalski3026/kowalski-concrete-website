e633e8ff0017ad0bb5e24571ec54652530746455
kowalski3026 <kowalski3026@gmail.com>
2026-05-13 09:52:55 -0400

    Update page.js

--- app/page.js
+++ app/page.js
@@ -114,7 +114,7 @@ export default function Home() {
   className="text-white py-24 bg-cover bg-center relative"
   style={{ backgroundImage: "url('/services-bg.jpg')" }}
 >
-  <div className="absolute inset-0 bg-black/45"></div>
+  <div className="absolute inset-0 bg-black/25"></div>
          <div className="relative z-10 max-w-7xl mx-auto px-6">
           <h2 className="text-5xl font-black text-center">
             CONCRETE SOLUTIONS FOR EVERY PROJECT
