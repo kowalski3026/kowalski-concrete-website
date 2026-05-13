caf89c02d1aa8a3a41fdce0b7fd4703703bc7568
kowalski3026 <kowalski3026@gmail.com>
2026-05-13 09:50:12 -0400

    Update page.js

--- app/page.js
+++ app/page.js
@@ -43,7 +43,7 @@ export default function Home() {
   }}
   >
         <div className="max-w-7xl mx-auto px-6 pt-48 pb-32 grid lg:grid-cols-2 gap-16 items-center">
-        <div className="bg-black/10 backdrop-blur-sm p-8 rounded-3xl shadow-2xl">
+        <div className="bg-black/45 backdrop-blur-sm p-8 rounded-3xl shadow-2xl">
             <h1 className="text-5xl md:text-7xl font-black leading-none tracking-tight text-white">
               BUILT ON STRENGTH.
               <span className="block text-amber-400">FINISHED WITH PRIDE.</span>
@@ -114,7 +114,7 @@ export default function Home() {
   className="text-white py-24 bg-cover bg-center relative"
   style={{ backgroundImage: "url('/services-bg.jpg')" }}
 >
-  <div className="absolute inset-0 bg-black/80"></div>
+  <div className="absolute inset-0 bg-black/45"></div>
          <div className="relative z-10 max-w-7xl mx-auto px-6">
           <h2 className="text-5xl font-black text-center">
             CONCRETE SOLUTIONS FOR EVERY PROJECT
