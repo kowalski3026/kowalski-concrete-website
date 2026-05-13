2e9a2ace101a6bc445f85af41532ef1898bf1c32
kowalski3026 <kowalski3026@gmail.com>
2026-05-13 14:05:59 -0400

    Update page.js

--- app/page.js
+++ app/page.js
@@ -37,12 +37,12 @@ export default function Home() {
      <main className="min-h-screen bg-zinc-100 text-zinc-900">
     <section
   <div className="absolute inset-0 bg-black/10"></div>
-  className="relative min-h-screen bg-black bg-cover bg-center text-white"
+  className="relative min-h-[1400px] md:min-h-screen bg-black bg-cover bg-center text-white"
   style={{
     backgroundImage: "url('/hero.jpg')",
   }}
 >
-    <div className="relative z-10 max-w-7xl mx-auto px-6 pt-32 pb-20 grid lg:grid-cols-2 gap-10 items-center">
+    <div className="relative z-10 max-w-7xl mx-auto px-6 pt-32 pb-48 grid lg:grid-cols-2 gap-10 items-center">
        <div className="p-6 rounded-3xl max-w-xl">
             <h1 className="text-5xl md:text-7xl font-black leading-none tracking-tight text-white">
               BUILT ON STRENGTH.
