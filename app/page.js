70db2b68788a067239491371e488c19d55356706
kowalski3026 <kowalski3026@gmail.com>
2026-05-14 08:45:41 -0400

    Update page.js

--- app/page.js
+++ app/page.js
@@ -164,8 +164,11 @@ export default function Home() {
                 
 </section>
               <section
-  className="relative py-24 bg-cover bg-center text-white"
-  style={{ backgroundImage: "url('/municipal-bg.jpg')" }}
+  className="relative py-24 bg-cover bg-center bg-fixed text-white"
+  style={{ backgroundImage: "url('/municipal-bg.jpg')",
+        backgroundSize: "cover",
+  backgroundPosition: "center",
+}}
 >
   <div className="absolute inset-0 bg-black/70"></div>
   <div className="relative z-10 max-w-7xl mx-auto px-6">
