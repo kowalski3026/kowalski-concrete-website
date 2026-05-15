5372786b1a7f9a421ec1a4703030733723981250
kowalski3026 <kowalski3026@gmail.com>
2026-05-14 21:24:13 -0400

    Update page.js

--- app/page.js
+++ app/page.js
@@ -235,7 +235,7 @@ export default function Home() {
     backgroundRepeat: "no-repeat",
   }}
 >
-  <div className="absolute inset-0 bg-black/75"></div>
+  <div className="absolute inset-0 bg-black/45"></div>
   <div className="relative z-10 max-w-7xl mx-auto px-6">
 
     <div className="text-center mb-16">
